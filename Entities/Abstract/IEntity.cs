ef1ed7e8289dc922afc2033da591bdd205f26142
Akın Topbaş <akin_topbas@hotmail.com>
2022-01-02 22:51:52 +0300

    Entity katmanında sınıflar eklendi

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Abstract
+{
+    /*
+     * Veritabanında karşılık gelen tablolarda olacak
+     */
+    public interface IEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Entities/Concrete/BaseEntities/AudittableEntity.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class AudittableEntity : BaseEntity, ICreatedEntity, IUpdatedEntity
+    {
+        public int CreatedUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? UpdatedUserId { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
