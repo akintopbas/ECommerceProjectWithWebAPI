cbd6ef22d18ab7636244770afef3d20b6cbef4e1
Akın Topbaş <akin_topbas@hotmail.com>
2022-01-08 21:49:39 +0300

    DataAccess katmanı kodlandı

---
+++ DataAccess/Concrete/Context/ECommerceProjectWithWebAPIContext.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Concrete.EntityFramework.Mapping;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.Context
+{
+    public class ECommerceProjectWithWebAPIContext: DbContext
+    {
+        public ECommerceProjectWithWebAPIContext(DbContextOptions<ECommerceProjectWithWebAPIContext> options):base(options)
+        {
+
+        }
+        public ECommerceProjectWithWebAPIContext()
+        {
+
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = ECommerceProjectWithWebAPIDb; Integrated Security = True;";
+            optionsBuilder.UseSqlServer(connString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
